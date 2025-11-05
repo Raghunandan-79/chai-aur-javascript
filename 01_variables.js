84aefb53e37ca02a46257281a8751eb7aa41b9cd
Raghunandan-19 <raghunandansharma1905@gmail.com>
2025-11-05 13:35:06 +0530

    Added 3 files

---
+++ 01_variables.js
@@ -0,0 +1,9 @@
+let age = 99;
+
+const price = 10;
+
+var classIn = "1st";
+
+console.log(age);
+console.log(price);
+console.log(classIn);
---
+++ 02_data_types.js
@@ -0,0 +1,29 @@
+/* 
+    modern js is different from old js now we have classes and objects in it
+    we use "use strict" by writing this it treat all js code as newer version
+*/
+
+"use strict";
+
+// alert(3 + 3) // we are using nodejs not browser
+// code readibility should be high
+// documentation --> mdn it is not original but good
+// original --> tc39.es --> ecma script. it is for them who write browsers
+
+// data types
+let name = "Hitesh"; // string
+let age = 18; // number
+let isLoggedIn = false; // boolean
+
+// number => 2^53 is range
+// bigint => for big numbers
+// string => "" or ''
+// boolean => true or false
+// null => standalone value. representation of an empty value, it is an object
+// undefined => when value is not defined
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
